546d2e012143bea0d03c32b8830a5f6459f517f8
SAURABH KUMAR <aiml23031@glbitm.ac.in>
2026-03-11 21:52:25 +0530

    Time: 0 ms (100.00%) | Memory: 8 MB (4.02%) - LeetSync

---
+++ 1054-complement-of-base-10-integer/complement-of-base-10-integer.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int bitwiseComplement(int n) {
+        if(n==0)
+        return 1;
+        int count =0;
+        int result=0;
+        while(n)
+        {   
+            int r=n%2;
+
+            result +=(pow(2,count)*!r);
+            count++;
+            n=n/2;
+        }
+        return result;
+    }
+};
